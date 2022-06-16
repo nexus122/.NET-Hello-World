b1f98deeee7ed96e45aa3d821df72d991ccf0a64
nexus122 <juan-pablo122@hotmail.es>
2022-06-16 09:04:18 +0200

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+/* Librerias */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Primer Hello World en C# -> .net */
+namespace HelloWorld
+{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
